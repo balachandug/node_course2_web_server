8227c6b7acc62746a122050fe3fb7e577d58506c
Balu <bgillell@calliduscloud.com>
2017-08-23 12:04:41 +0530

    setup start script and heroku port

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const hbs = require('hbs');
 const fs = require('fs');
 var Client = require('node-rest-client').Client;
+var port = process.env.PORT || 3000;
 var app = express();
 var client = new Client();
 var products;
@@ -80,6 +81,6 @@ app.get('/bad', (req, res) => {
 	});
 });
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000');
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
 });
